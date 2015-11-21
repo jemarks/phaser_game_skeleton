a00ac241bd753cb22ef08d3f57f9f8ca92ea58d6
amattoni <amattoni@petrichor.io>
2015-11-21 11:14:14 -0500

    Initial skeleton of project

--- README.md
+++
@@ -1,2 +0,0 @@
-# phaser_game_skeleton
-Files and structure to start a phaser game!
---
+++ src/js/states/splash.js
@@ -0,0 +1,11 @@
+var Splash = function () {
+
+};
+
+module.exports = Splash;
+
+Splash.prototype = {
+  create: function () {
+
+  }
+};
